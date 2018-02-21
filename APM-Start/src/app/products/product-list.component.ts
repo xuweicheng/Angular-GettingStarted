0bc55e6a655e9a3569bf5a59e8bd8eccab51bee8
xuweicheng <weicheng.xu@gmail.com>
2018-02-21 12:23:52 -0800

    add product service retrieve data with http

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'pm-products',
@@ -11,6 +12,7 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
+    errorMessage: string;
 
     _listFilter: string = 'cart';
     get listFilter(): string {
@@ -22,32 +24,10 @@ export class ProductListComponent implements OnInit {
     }
 
     filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-            'productId': 1,
-            'productName': 'Leaf Rake',
-            'productCode': 'GDN-0011',
-            'releaseDate': 'March 19, 2016',
-            'description': 'Leaf rake with 48-inch wooden handle.',
-            'price': 19.95,
-            'starRating': 3.2,
-            'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png'
-        },
-        {
-            'productId': 2,
-            'productName': 'Garden Cart',
-            'productCode': 'GDN-0023',
-            'releaseDate': 'March 18, 2016',
-            'description': '15 gallon capacity rolling garden cart',
-            'price': 32.99,
-            'starRating': 4.2,
-            'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png'
-        }
-    ];
+    products: IProduct[] = [];
 
-    constructor() {
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart';
+    constructor(private _productService: ProductService) {
+        // this.listFilter = 'cart';
     }
 
     performFilter(filterBy: string): IProduct[] {
@@ -61,7 +41,14 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        console.log('In OnInit');
+        this._productService.getProducts()
+            .subscribe(
+                products => {
+                    this.products = products;
+                    this.filteredProducts = this.products;
+                },
+                error => this.errorMessage = <any>error
+            );
     }
 
     onRatingClicked(message: string): void {
